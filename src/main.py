037b0cf0ccd6ec677b4420c5d0ea88a28ede13b7
Christoph Scheingraber <chris@mytum.de>
2018-04-27 08:09:11 +0200

    Basic Project List View.

--- src/main.py
+++ src/main.py
@@ -1,40 +1,163 @@
+"""
+3PM
+=====
+Python Pomodoro Project Manager - P.P.P.M.- 3PM - so you too can leave the office every day by 3pm!
+"""
+import json
+from os.path import join, exists
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.properties import NumericProperty, StringProperty
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+from kivy.properties import ListProperty, StringProperty, \
+        NumericProperty, BooleanProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.clock import Clock
+from kivy.uix.label import Label
+
+__version__ = '0.1.1'
 
 
-class MainWidget(GridLayout):
-    minutes = NumericProperty(35)
-    seconds = NumericProperty()
-    timeString = StringProperty()
+class MutableTextInput(FloatLayout):
+
+    text = StringProperty()
+    multiline = BooleanProperty(True)
 
     def __init__(self, **kwargs):
-        super(MainWidget, self).__init__(**kwargs)
-        Clock.schedule_interval(self.decrement_time, 1)
-        self.decrement_time(0)
-        self.timeString = str(self.minutes) + ':' + str(self.seconds)
+        super(MutableTextInput, self).__init__(**kwargs)
+        Clock.schedule_once(self.prepare, 0)
 
-    def decrement_time(self, interval):
-        self.seconds -= 1
-        if self.seconds < 0:
-            self.minutes -= 1
-            self.seconds = 59
-        self.timeString = str(self.minutes) + ':' + str(self.seconds)
+    def prepare(self, *args):
+        self.w_textinput = self.ids.w_textinput.__self__
+        self.w_label = self.ids.w_label.__self__
+        self.view()
 
-    def start(self):
-        Clock.unschedule(self.decrement_time)
-        Clock.schedule_interval(self.decrement_time, 1)
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos) and touch.is_double_tap:
+            self.edit()
+        return super(MutableTextInput, self).on_touch_down(touch)
 
-    def stop(self):
-        self.minutes = 35
-        self.seconds = 0
-        Clock.unschedule(self.decrement_time)
+    def edit(self):
+        self.clear_widgets()
+        self.add_widget(self.w_textinput)
+        self.w_textinput.focus = True
+
+    def view(self):
+        self.clear_widgets()
+        if not self.text:
+            self.w_label.text = "Double tap/click to edit"
+        self.add_widget(self.w_label)
+
+    def check_focus_and_view(self, textinput):
+        if not textinput.focus:
+            self.text = textinput.text
+            self.view()
 
 
-class MainApp(App):
+class ProjectView(Screen):
+
+    project_index = NumericProperty()
+    project_title = StringProperty()
+    project_content = StringProperty()
+
+
+class ProjectListItem(BoxLayout):
+
+    def __init__(self, **kwargs):
+        print(kwargs)
+        del kwargs['index']
+        super(ProjectListItem, self).__init__(**kwargs)
+    project_content = StringProperty()
+    project_title = StringProperty()
+    project_index = NumericProperty()
+
+
+class Projects(Screen):
+
+    data = ListProperty()
+
+    def args_converter(self, row_index, item):
+        return {
+            'project_index': row_index,
+            'project_content': item['content'],
+            'project_title': item['title']}
+
+
+class ProjectApp(App):
+
     def build(self):
-        return MainWidget()
+        self.projects = Projects(name='projects')
+        self.load_projects()
 
-MainApp().run()
+        self.transition = SlideTransition(duration=.35)
+        root = ScreenManager(transition=self.transition)
+        root.add_widget(self.projects)
+        return root
+
+    def load_projects(self):
+        if not exists(self.projects_fn):
+            return
+        with open(self.projects_fn) as fd:
+            data = json.load(fd)
+        self.projects.data = data
+
+    def save_projects(self):
+        with open(self.projects_fn, 'w') as fd:
+            json.dump(self.projects.data, fd)
+
+    def del_project(self, project_index):
+        del self.projects.data[project_index]
+        self.save_projects()
+        self.refresh_projects()
+        self.go_projects()
+
+    def edit_project(self, project_index):
+        project = self.projects.data[project_index]
+        name = 'project{}'.format(project_index)
+
+        if self.root.has_screen(name):
+            self.root.remove_widget(self.root.get_screen(name))
+
+        view = ProjectView(
+            name=name,
+            project_index=project_index,
+            project_title=project.get('title'),
+            project_content=project.get('content'))
+
+        self.root.add_widget(view)
+        self.transition.direction = 'left'
+        self.root.current = view.name
+
+    def add_project(self):
+        self.projects.data.append({'title': 'New Project', 'content': ''})
+        project_index = len(self.projects.data) - 1
+        self.edit_project(project_index)
+
+    def set_project_content(self, project_index, project_content):
+        self.projects.data[project_index]['content'] = project_content
+        data = self.projects.data
+        self.projects.data = []
+        self.projects.data = data
+        self.save_projects()
+        self.refresh_projects()
+
+    def set_project_title(self, project_index, project_title):
+        self.projects.data[project_index]['title'] = project_title
+        self.save_projects()
+        self.refresh_projects()
+
+    def refresh_projects(self):
+        data = self.projects.data
+        self.projects.data = []
+        self.projects.data = data
+
+    def go_projects(self):
+        self.transition.direction = 'right'
+        self.root.current = 'projects'
+
+    @property
+    def projects_fn(self):
+        return join(self.user_data_dir, 'projects.json')
+
+
+if __name__ == '__main__':
+    ProjectApp().run()
---
+++ src/timer/main.py
@@ -0,0 +1,41 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import NumericProperty, StringProperty
+from kivy.uix.gridlayout import GridLayout
+from kivy.clock import Clock
+
+
+class MainWidget(GridLayout):
+    minutes = NumericProperty(35)
+    seconds = NumericProperty()
+    timeString = StringProperty()
+
+    def __init__(self, **kwargs):
+        super(MainWidget, self).__init__(**kwargs)
+        # Clock.schedule_interval(self.decrement_time, 1)
+        # self.decrement_time(0)
+        self.timeString = str(self.minutes) + ':' + str(self.seconds)
+
+    def decrement_time(self, interval):
+        self.seconds -= 1
+        if self.seconds < 0:
+            self.minutes -= 1
+            self.seconds = 59
+        self.timeString = str(self.minutes) + ':' + str(self.seconds)
+
+    def start(self):
+        Clock.unschedule(self.decrement_time)
+        Clock.schedule_interval(self.decrement_time, 1)
+
+    def stop(self):
+        self.minutes = 35
+        self.seconds = 0
+        self.timeString = str(self.minutes) + ':' + str(self.seconds)
+        Clock.unschedule(self.decrement_time)
+
+
+class MainApp(App):
+    def build(self):
+        return MainWidget()
+
+MainApp().run()
