52ed1975918d8b08093e038337685c195ea7bd80
Christoph Scheingraber <CScheingraber@munichre.com>
2018-05-29 14:15:22 +0200

    Rename project to task.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
 """
-3PM
-=====
-Python Pomodoro Project Manager - PPPM - 3PM - so you too can leave the office every day by 3pm!
+3PM - so you too can leave the office every day by 3pm!
+
+Copyright (C) 2018 Chris Scheingraber
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+
 import json
 from os.path import join, exists
 from kivy.app import App
