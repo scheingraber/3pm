88c11c625af20fe7fff568fb94434f1b127a4c11
Christoph Scheingraber <chris@mytum.de>
2018-06-14 09:35:48 +0200

    Refactor: extract all project view related classes.

--- src/main.py
+++ src/main.py
@@ -18,19 +18,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 # need to set kivy configs before importing anything else
 from kivy.config import Config
-
-from src.timer import Timer
-
 Config.set('kivy', 'exit_on_escape', 0)
-# other imports
+
+# import 3PM classes
+from timer import Timer
+from projectsview import Projects, ProjectView, ProjectViewWithoutNotepad, ProjectViewSimple,\
+                         ProjectViewSimpleWithoutNotepad, QuickView
+
+# third party imports
 import json
 from os.path import join, exists
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
-from kivy.properties import ListProperty, StringProperty, \
-        NumericProperty, BooleanProperty
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import ScreenManager, SlideTransition
+from kivy.properties import StringProperty
 from kivy.clock import Clock
 from kivy.uix.settings import SettingsWithTabbedPanel
 from settings_info import timer_settings_json, ebs_settings_json
@@ -43,108 +43,6 @@ elif platform == 'win':
     from infi.systray import SysTrayIcon
 
 __version__ = '0.6.4'
-
-
-class MutableTextInput(FloatLayout):
-    text = StringProperty()
-    multiline = BooleanProperty(True)
-
-    def __init__(self, **kwargs):
-        super(MutableTextInput, self).__init__(**kwargs)
-        Clock.schedule_once(self.prepare, 0)
-
-    def prepare(self, *args):
-        self.w_textinput = self.ids.w_textinput.__self__
-        self.w_label = self.ids.w_label.__self__
-        self.view()
-
-    def on_touch_down(self, touch):
-        if self.collide_point(*touch.pos) and touch.is_double_tap:
-            self.edit()
-        return super(MutableTextInput, self).on_touch_down(touch)
-
-    def edit(self):
-        self.clear_widgets()
-        self.add_widget(self.w_textinput)
-        self.w_textinput.focus = True
-
-    def view(self):
-        self.clear_widgets()
-        if not self.text:
-            self.w_label.text = "Double tap/click to edit"
-        self.add_widget(self.w_label)
-
-    def check_focus_and_view(self, textinput):
-        if not textinput.focus:
-            self.text = textinput.text
-            self.view()
-
-
-class ProjectView(Screen):
-    project_index = NumericProperty()
-    project_title = StringProperty()
-    project_content = StringProperty()
-    project_logged = NumericProperty()
-    project_estimated = NumericProperty()
-    project_quartiles = StringProperty()
-
-
-class ProjectViewWithoutNotepad(Screen):
-    project_index = NumericProperty()
-    project_title = StringProperty()
-    project_logged = NumericProperty()
-    project_estimated = NumericProperty()
-    project_quartiles = StringProperty()
-
-
-class ProjectViewSimple(Screen):
-    project_index = NumericProperty()
-    project_title = StringProperty()
-    project_content = StringProperty()
-
-
-class ProjectViewSimpleWithoutNotepad(Screen):
-    project_index = NumericProperty()
-    project_title = StringProperty()
-
-
-class QuickView(Screen):
-    project_content = StringProperty()
-
-
-class ProjectListItem(BoxLayout):
-    def __init__(self, **kwargs):
-        # print(kwargs)
-        del kwargs['index']
-        super(ProjectListItem, self).__init__(**kwargs)
-    project_content = StringProperty()
-    project_title = StringProperty()
-    project_index = NumericProperty()
-    project_logged = NumericProperty()
-    project_estimated = NumericProperty()
-    project_progress = StringProperty()
-
-
-class Projects(Screen):
-    data = ListProperty()
-
-    def __init__(self, name, config):
-        super(Projects, self).__init__(name=name)
-        self.use_ebs = config.get('ebs', 'use_ebs') == '1'
-
-    def args_converter(self, row_index, item):
-        if self.use_ebs:
-            project_progress_str = '%.f%% (%i/%i)' % (item['logged']*100./item['estimated'], item['logged'], item['estimated'])
-        else:
-            project_progress_str = ''
-
-        return {
-            'project_index': row_index,
-            'project_content': item['content'],
-            'project_title': item['title'],
-            'project_logged': item['logged'],
-            'project_estimated': item['estimated'],
-            'project_progress': project_progress_str}
 
 
 class ProjectApp(App):
